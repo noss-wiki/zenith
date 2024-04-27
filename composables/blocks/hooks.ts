029e66ed445bceca994def2e288e872b2680d4ec
Codefoxdev <admin@codefoxdev.com>
2024-04-27 22:06:41 +0200

    Added string values to FocusReason enum

--- composables/blocks/hooks.ts
+++ composables/blocks/hooks.ts
@@ -2,25 +2,25 @@ export enum FocusReason {
   /**
    * When this block is created and inserted in the editor.
    */
-  Insert,
+  Insert = 'insert',
   /**
    * When a previous block is deleted and this block gets focussed because of it.
    */
-  Delete,
+  Delete = 'delete',
   /**
    * When the last block is deleted and this is the new last block.
    */
-  DeleteLast,
+  DeleteLast = 'deleteLast',
   /**
    * When arrow keys were used from the previous block to travel (downwards) into this block.
    *
    * Keys: Down, Right
    */
-  ArrowNext,
+  ArrowNext = 'arrowNext',
   /**
    * When arrow keys were used from the next block to travel (upwards) into this block.
    *
    * Keys: Up, Left
    */
-  ArrowPrevious,
+  ArrowPrevious = 'arrowPrevious',
 }
